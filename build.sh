81dcbecff8874c3aa37d4aa844addd5fedca71da
Alexey Zhokhov <alexey@zhokhov.com>
2015-12-26 02:55:05 +0800

    Doesn't need to push to Hub from CircleCI.

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ echo "> 1. Building Docker image"
 echo ""
 docker build -t donbeave/php-fpm:$VERSION .
 
-echo ""
-echo "> 2. Publishing Docker image to Docker Hub"
-echo ""
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push donbeave/php-fpm:$VERSION
+#echo ""
+#echo "> 2. Publishing Docker image to Docker Hub"
+#echo ""
+#docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+#docker push donbeave/php-fpm:$VERSION
